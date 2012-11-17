296fa97d716db5603653105dc4d8bdd8d49e9c02
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2012-11-17 19:13:42 +0000

    Refactor ownership of path constants

--- mangler.php
+++ mangler.php
@@ -1,7 +1,9 @@
 <?php
 
 setlocale(LC_ALL, 'en_GB.UTF8');
+date_default_timezone_set('UTC');
 
-define('PROJECT_NAME', 'mangler');
+define('TOP_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+define('PROJECT_PATH', TOP_PATH . 'mangler' . DIRECTORY_SEPARATOR);
 
 require('acorn/bootstrap.php');
